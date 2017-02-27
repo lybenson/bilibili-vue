56dd0ddf657bb73b3a41519f0c0baace20e6ac0b
lybenson <naivebenson@gmail.com>
2017-02-28 00:54:32 +0800

    api总结

--- bilibili-api/app/routes/banner.js
+++ bilibili-api/app/routes/banner.js
@@ -4,7 +4,7 @@ const router = Router({
   prefix: '/banner'
 })
 router.post('/list', async (ctx, next) => {
-  let response = await axios.get('http://api.bilibili.com/x/web-show/res/loc?callback=jQuery17205969745067413896_1482805801285&jsonp=jsonp&pf=0&id=23&_=1482805801599')
+  let response = await axios.get('')
   ctx.body = response
 })
 
--- bilibili-api/app/routes/urlConfig.js
+++ bilibili-api/app/routes/urlConfig.js
@@ -1,3 +1,6 @@
+// 各分类内容
+export const ding = 'http://www.bilibili.com/index/ding.json'
+
 // 轮播图
 export const banner = 'http://api.bilibili.com/x/web-show/res/loc?callback=jQuery17205969745067413896_1482805801285&jsonp=jsonp&pf=0&id=23&_=1482805801599'
 
@@ -18,3 +21,38 @@ export const promoteAd = 'http://api.bilibili.com/x/web-show/res/loc?callback=jQ
 
 // 直播
 export const liveXhr = 'http://live.bilibili.com/bili/recom?callback=liveXhrDone'
+
+// 番剧下方更新列表右侧新番放送表上侧
+export const slideshow = 'http://www.bilibili.com/index/slideshow/41.json'
+// 番剧下方更新列表右侧新番放送表下侧
+export const season = 'http://www.bilibili.com/api_proxy?app=bangumi&action=get_season_by_tag&page=1&tag_id=101&pagesize=4'
+
+
+// 轮播图右边总排行
+// 3日排行
+export const ranking3 = 'http://www.bilibili.com/index/ranking-3day.json'
+// 7日排行
+export const ranking7 = 'http://www.bilibili.com/index/ranking-week.json'
+// 昨日排行
+export const randking = 'http://www.bilibili.com/index/ranking.json'
+
+// 最底部特别推荐
+export const recommend = 'http://www.bilibili.com/index/recommend.json'
+
+// 各分类下3日排行 http://www.bilibili.com/index/catalogy/
+// 动画排行 1-3day.json
+// 番剧排行 13-3day.json
+// 音乐排行 3-3day.json
+// 舞蹈排行 129-3day.json
+// 游戏排行 3-3day.json
+// 科技排行 36-3day.json
+// 生活排行 160-3day.json
+// 鬼畜排行 119-3day.json
+// 时尚排行 155-3day.json
+// 广告排行 165-3day.json
+// 娱乐排行 5-3day.json
+// 电影排行 23-3day.json
+// TV剧集排行 11-3day.json
+
+// 各分类下7日排行
+// 动画排行 1-week.json
